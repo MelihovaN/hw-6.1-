7c5bd91cfa511d89fb133d7225667f3bc8496f17
n.melihova <melihova.qa@gmail.com>
2023-01-23 19:52:49 +0200

    Added comments

--- calculations.js
+++ calculations.js
@@ -18,3 +18,9 @@ console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
+
+/* изменила внешний вид формул, 
+удалила команду console.log внутри функций,
+удалила лишнее число (4000) в третьей команде consol.log,
+команды console.log можно оставить внизу, 
+а можно перенести соотвественно под каждую функцию  /*
